f7cd42d6457024d261426e8b31e7cc9f3ad7a2b3
jaeil759 <jaeil759@gmail.com>
2020-08-17 18:22:02 +0900

    2020.08.17

--- servletSite/src/com/itwill/servlet/AddressInsertActionServlet.java
+++ servletSite/src/com/itwill/servlet/AddressInsertActionServlet.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.itwill.address.Address;
+import com.itwill.address.AddressSevice;
+
 /**
  * Servlet implementation class AddressInsertActionServlet
  */
@@ -25,18 +28,34 @@ public class AddressInsertActionServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
 		/*
+		 * 0. 요청객체 encoding 설정
 		 * 1. 파라메타 바끼
 		 * 2. AddressService객체생성
 		 * 3. 파라메타데이타로 Address객체 생성
 		 * 4. AddressService.create()메소드 실행
 		 * 5. address_list.do 로 redirection
 		 */
+		request.setCharacterEncoding("UTF-8");
 		String id = request.getParameter("id");
 		String name = request.getParameter("name");
-		String gender = request.getParameter("gender");
-		String address= request.getParameter("addr");
-		
+		String phone = request.getParameter("phone");
+		String address = request.getParameter("address");
+		Address recvAddress=new Address(id, name, phone, address);
+		AddressSevice addressSevice=new AddressSevice();
+		addressSevice.create(recvAddress);
+		int insertRowcount = addressSevice.create(recvAddress);
+		response.sendRedirect("address_list.do");
+	}catch (Exception e) {
+		e.printStackTrace();
+		response.sendRedirect("address_error.html");
 	}
+	
+}
 
 }
+
+
+
+
--- servletSite/src/com/itwill/servlet/AddressListServlet.java
+++ servletSite/src/com/itwill/servlet/AddressListServlet.java
@@ -2,12 +2,17 @@ package com.itwill.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.itwill.address.Address;
+import com.itwill.address.AddressSevice;
+
 
 /**
  * Servlet implementation class AddressListServlet
@@ -16,7 +21,10 @@ import javax.servlet.http.HttpServletResponse;
 public class AddressListServlet extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
+		try {
 		PrintWriter out=response.getWriter();
+		AddressSevice addressService = new AddressSevice(); 
+		ArrayList<Address> addressList = addressService.selectAll();
 		out.println("<!DOCTYPE html>");
 		out.println("<html>");
 		out.println("<head>");
@@ -26,17 +34,23 @@ public class AddressListServlet extends HttpServlet {
 		out.println("<body>");
 		out.println("<h1>[주소록리스트]</h1><hr>");
 		out.println("<div>");
-		out.println("	<a href=''>[주소록쓰기]</a>");
+		out.println("	<a href='address_insert_form.html'>[주소록쓰기폼]</a>");
 		out.println("</div>");
 		out.println("<div>");
 		out.println("	<ul>");
-		out.println("		<li><a href='address_detail.do?no=1'>1 KIM </a></li>");
-		out.println("		<li><a href='address_detail.do?no=3'>3 LEE </a></li>");
-		out.println("		<li><a href='address_detail.do?no=7'>7 KIM </a></li>");
+		//out.println("		<li><a href='address_detail.do?no=1'>1 KIM </a></li>");
+		for(Address address:addressList) {
+			out.println("<li><a href='address_detail.do?no="+address.getNo()+"'>["+address.getNo()+"]"+address.getName()+"</a></li>");
+		}
+		//out.println("		<li><a href='address_detail.do?no=7'>7 KIM </a></li>");
 		out.println("	</ul>");
 		out.println("</div>");
 		out.println("</body>");
 		out.println("</html>");
+		}catch (Exception e) {
+			e.printStackTrace();
+			response.sendRedirect("address_error.html");
+		}
 	}
 
 }
